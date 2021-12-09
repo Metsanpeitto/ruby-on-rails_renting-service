6bbc81924404172a0cae7872b4d26e26632ea03b
Metsanpeitto <abraham@waldenberginc.com>
2021-12-10 00:42:50 +0100

    Add seed and fix migration order

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ user  = User.create(name:"kj",email:"ls2@dls.com",password:"123456",password_confirmation:"123456")
+ 
+ Item.create([{name:"Big boat",description:"It is amazing",image:"https://i.imgur.com/SZJIKne.png",price:300,color:"blue", user_id: user.id},
+    {name:"Medium boat",description:"It is less amazing",image:"https://i.imgur.com/ZneOae2.png",price:150,color:"red", user_id: user.id},
+    {name:"Small boat",description:"It is small, but amazing",image:"https://i.imgur.com/RdEZTBM.png" ,price:100,color:"green", user_id: user.id}
+    ])
