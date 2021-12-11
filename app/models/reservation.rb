8b50352fb1e80543c9fb222e8c50e599ebb602e1
Metsanpeitto <abraham@waldenberginc.com>
2021-12-11 03:55:23 +0100

    Remove the dependent: :destroy from reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,6 @@ class Reservation < ApplicationRecord
   validates :user_id, numericality: { only_integer: true, greater_than: 0 }, presence: true
   validates :item_id, numericality: { only_integer: true, greater_than: 0 }, presence: true
 
-  belongs_to :item, dependent: :destroy
+  belongs_to :item
   belongs_to :user
 end
