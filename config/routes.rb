a2da9a75dfb0d60d583255e598a1b244260909fd
Metsanpeitto <abraham@waldenberginc.com>
2021-12-14 14:08:13 +0100

    Add root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do  
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  root to: "items/index"
   get 'current_user/index'
+  
   devise_for :users, defaults: { format: :json }, path: '',
             path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' } 
   resources :reservations
