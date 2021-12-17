cad322b18ee26d39fcaf3780d54b67792e9830a7
Metsanpeitto <abraham@waldenberginc.com>
2021-12-17 10:05:39 +0100

    Fix cors initializer

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://friendly-hugle-cbed27.netlify.app/', 'http://localhost:3001', 'http://127.0.0.1:3001'
+    origins 'https://friendly-hugle-cbed27.netlify.app', 'http://localhost:3001', 'http://127.0.0.1:3001'
     resource '*', headers: %w[Authorization],
                   expose: %w[Authorization], methods: :any
   end
